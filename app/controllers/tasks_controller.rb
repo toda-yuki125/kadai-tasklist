e2039ec41255be64e3ac9563ac16e76df2f013ae
yuki.toda <pyan.owl@gmail.com>
2017-07-10 13:20:40 +0000

    Heroku third

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :create, :update, :edit]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -43,7 +44,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to @task
   end
 
 private
@@ -56,5 +57,13 @@ private
   def task_params
     params.require(:task).permit(:content,:status)
   end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+
 
 end
